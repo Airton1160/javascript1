82c640a6350d92ef9dcec6334da223af2adb9350
Airton diniz <liriungo@outlook.com.br>
2020-04-08 13:16:26 -0300

    modificacao01

---
+++ aplicacao01/Data/aplicacao01Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using aplicacao01.Models;
+
+namespace aplicacao01.Data
+{
+    public class aplicacao01Context : DbContext
+    {
+        public aplicacao01Context (DbContextOptions<aplicacao01Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<aplicacao01.Models.Departamento> Departamento { get; set; }
+    }
+}
--- aplicacao01/Startup.cs
+++ aplicacao01/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using aplicacao01.Data;
 
 namespace aplicacao01
 {
@@ -33,6 +35,9 @@ namespace aplicacao01
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<aplicacao01Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("aplicacao01Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
